8321522f206c55609d78e0ed0a496f1a533fb710
Renata <127457027+RenataSch@users.noreply.github.com>
2023-12-08 09:27:51 -0300

    Add files via upload

---
+++ O.O/07_12/exMusica/Main2.java
@@ -0,0 +1,6 @@
+public class Main2{
+    public static void main(String[]args){
+        StockKeeper johnDoe = new StockKeeper("John Doe");
+        johnDoe.manageAlbum(new Album(), "Slippery When Wet", "Bon Jovi", -1000.00, -50);
+    }
+}
